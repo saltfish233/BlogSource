0f95ad09ba9e6f6430f01a67b8ae5500bcaa7407
saltfish233 <1603321645@qq.com>
2022-07-28 11:32:22 +0800

    feat: 新增图片

--- push.bat
+++ push.bat
@@ -1,2 +1,24 @@
-cd /d %~dp0
-git add . && git commit -m "feat: 新增博客图片" && git pull && git push
+@echo off
+
+title GIT一键提交
+color 3
+echo 当前目录是：%cd%
+echo;
+ 
+echo 开始添加变更：git add .
+git add .
+echo;
+ 
+set /p declation=feat: 新增图片
+git commit -m "%declation%"
+echo;
+ 
+echo 将变更情况提交到远程自己分支
+git push origin master
+echo;
+
+echo 执行完毕！
+echo;
+ 
+pause
+
